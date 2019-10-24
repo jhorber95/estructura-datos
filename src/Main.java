adca1c21720f753b73cd47b3cf70d1babe54170f
jhorkman bernal <jhorber95@gmail.com>
2019-10-24 11:32:34 -0500

    count total tree nodes

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String [] args) {
+
+        Tree tree = new Tree();
+
+        tree.insert(99);
+        tree.insert(39);
+        tree.insert(59);
+        tree.insert(129);
+        tree.insert(259);
+        tree.insert(249);
+        tree.insert(279);
+        tree.insert(219);
+        tree.insert(209);
+
+        tree.countNodes(tree.root);
+        System.out.println(tree.totalNodes);
+
+    }
+}
